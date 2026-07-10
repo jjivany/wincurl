792e9a6dff7e06ce21b97699b2d8879d6a128b87
Jason <jayjayivany@gmail.com>
2026-07-10 01:05:38 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+    name='wincurl3',
+    version='20.0.5',
+    description='WinCurl 3 - A Multiplayer Curling Simulator',
+    author='Jason',
+    packages=['wincurl3'],
+    package_dir={'wincurl3': 'wincurl_android'},
+    include_package_data=True,
+    install_requires=[
+        'pygame-ce>=2.5.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'wincurl3=wincurl3.main:main',
+        ],
+    },
+    python_requires='>=3.8',
+)
