9adec51b295051f10f82e2b1c17ad705fb583bf9
Jason <jayjayivany@gmail.com>
2026-07-12 20:37:36 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='wincurl3',
-    version='24.3',
+    version='25.0',
     description='WinCurl 3 - A Multiplayer Curling Simulator',
     author='Jason',
     packages=['wincurl3'],
