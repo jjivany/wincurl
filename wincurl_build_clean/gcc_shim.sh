5e5b615b1c016d3cc8405c41e76d7a295329a663
Jason <jayjayivany@gmail.com>
2026-07-19 10:25:41 -0300

    Delete wincurl_build_clean directory

--- wincurl_build_clean/gcc_shim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# If the script calls gcc with -V or -qversion, redirect it to --version
-if [[ "$*" == *"-V"* ]] || [[ "$*" == *"-qversion"* ]]; then
-    exec /usr/bin/gcc --version
-else
-    exec /usr/bin/gcc "$@"
-fi
