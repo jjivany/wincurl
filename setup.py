76ffd8cfe75ec28d2856eac37f904079af8ffee3
jason <jayjayivany@gmail.com>
2026-08-11 10:04:56 -0300

    Bump version to 88

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='wincurl3',
-    version="3.0.87",
+    version="3.0.88",
     description='WinCurl 3 - A Multiplayer Curling Simulator',
     author='Jason Ivany',
     packages=['wincurl3'],
