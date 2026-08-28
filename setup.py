fcbe20a339333be4ae3d96a5dda1cef657f39290
jason <jayjayivany@gmail.com>
2026-08-28 07:45:50 -0300

    Update APK and package info for 116

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 setup(
     name='wincurl3',
-    version='114',
+    version='116',
     description='WinCurl 3 - A Multiplayer Curling Simulator',
     author='Jason Ivany',
     packages=['wincurl3'],
     package_dir={'wincurl3': '.'},
     include_package_data=True,
-    package_data={'wincurl3': ['*.jpg', '*.wav', '*.png', '*.ttf']},
+    package_data={'wincurl3': ['*.jpg', '*.ogg', '*.png', '*.ttf']},
     install_requires=[
         'pygame-ce>=2.5.0',
     ],
